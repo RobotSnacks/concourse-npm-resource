c351388d4dd14217cb257696dec105c95c5df189
Joshua Toenyes <josh@robotsnacks.io>
2018-05-08 19:23:20 -0600

    feat(out): update to use NPM_TOKEN param w/ .npmrc file

--- out.js
+++ out.js
@@ -1,62 +1,81 @@
 #!/usr/bin/env node
 
-var exec = require("child_process").exec,
-  fs = require("fs")
+const exec = require("child_process").exec;
+const fs = require("fs");
 
-process.stdin.on("data", (chunk) => {
-  const data = JSON.parse(chunk)
-  const source = data.source
-  if(!source) {
-    console.error("Please specify a source.")
-    process.exit(1)
-  }
-  const token = source.token
-  if(!token) {
-    console.error("Please specify `source.token`.")
-    process.exit(1)
-  }
-  const params = data.params
-  if(!params) {
-    console.error("Please specify `params`.")
-    process.exit(1)
-  }
-  const npmrc = fs.createWriteStream("/root/.npmrc")
-  npmrc.write(`//registry.npmjs.org/:_authToken=${token}`)
-  npmrc.close()
+process.stdin.on("data", chunk => {
+  const data = JSON.parse(chunk);
 
-  const path = params.path
-  if(!path) {
-    console.error("Please set `params.path`.")
-    process.exit(1)
+  const source = data.source;
+  const token = source.token;
+  const params = data.params;
+
+  if (!source) {
+    console.error("Please specify a source.");
+    process.exit(1);
   }
-  var cmdLine = "npm publish"
-  const access = params.access
-  if(access && ["public", "restricted"].indexOf(access) == -1) {
-    console.error("Specified access ("+access+") is neither `public` nor `restricted`.")
-    process.exit(1)
-  } else if(access)
-    cmdLine += ` --access ${access}`
-  const cwd = `${process.argv[2]}/${path}`
-  exec(cmdLine, {cwd: cwd}, (err, stdout, stderr) => {
-    if(err) {
-      console.error(err)
-      process.exit(1)
+
+  if (!token) {
+    console.error("Please specify `source.token`.");
+    process.exit(1);
+  }
+
+  if (!params) {
+    console.error("Please specify `params`.");
+    process.exit(1);
+  }
+
+  const path = params.path;
+
+  if (!path) {
+    console.error("Please set `params.path`.");
+    process.exit(1);
+  }
+
+  var cmdLine = "/usr/local/bin/npm publish";
+  const access = params.access;
+
+  const cwd = `${process.argv[2]}/${path}`;
+
+  console.error("all files in target directory...");
+  fs.readdir(cwd, (err, files) => {
+    files.forEach(file => {
+      console.error(file);
+    });
+  });
+
+  const opts = {
+    cwd,
+    env: {
+      ...process.env,
+      NPM_TOKEN: token
     }
-    const lines = stdout.split("\n").filter((v) => v)
-    const line = lines[lines.length-1]
-    const tokens = line.split("@")
-    if(tokens.length > 1) {
-      const version = tokens[tokens.length-1]
-      if(version) {
-        console.log(JSON.stringify({version: {number: version}}))
-        process.exit(0)
+  };
+
+  console.error("getting ready to exec", cmdLine);
+  exec(cmdLine, opts, (err, stdout, stderr) => {
+    if (err) {
+      console.error("trying to run", cmdLine);
+      console.error("got error in child process...", err);
+      process.exit(1);
+    }
+
+    console.error(stdout);
+
+    const lines = stdout.split("\n").filter(v => v);
+    const line = lines[lines.length - 1];
+    const tokens = line.split("@");
+
+    if (tokens.length > 1) {
+      const version = tokens[tokens.length - 1];
+      if (version) {
+        console.log(JSON.stringify({ version: { number: version } }));
+        process.exit(0);
       } else {
-        console.error("Didn't get a version.", lines, line)
-        process.exit(1)
+        process.exit(1);
       }
     } else {
-      console.error(`Don't know what this output means: ${stdout.toString()}`)
-      process.exit(1)
+      process.exit(1);
     }
-  })
-})
+  });
+});
